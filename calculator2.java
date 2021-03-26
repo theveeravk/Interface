1abd2549f72ecc95df9feefbe0056155a2248a20
veerakumar <veeravkmsccs@gmail.com>
2021-03-26 04:38:01 -0700

    method calling example

---
+++ calculator2.java
@@ -0,0 +1,16 @@
+public class calculator2
+{
+   public static void main(String[] args)
+   {
+      calculator2 calc = new calculator2();
+	 int total =  calc.sub();
+	System.out.println(total);
+	  
+   }
+   public int sub()
+   {
+     int no1 = 100,no2 =4; 
+	 
+	  return no1-no2;
+   }
+}
