8e28a96dc3128d7897771ba798ba8e9f11e218c6
veerakumar <veeravkmsccs@gmail.com>
2021-03-26 04:25:21 -0700

    method calling sample program

---
+++ Calculator.java
@@ -0,0 +1,18 @@
+public class Calculator
+{
+  public static void main(String[] args)
+  {
+    Calculator calc = new Calculator();
+	
+	double result = calc.add();
+	System.out.println(result);
+  }
+  
+   public double add()
+   {
+     int no1 =100, no2 =200;
+	 //int total = no1+no2;
+	 return 100+20.5;
+	 
+   }
+}
