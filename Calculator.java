b13024fa8cdbb93fecea66271fe7ef4d8c3c00f4
veerakumar <veeravkmsccs@gmail.com>
2021-03-29 23:56:41 -0700

    methodoverloading

--- Calculator.java
+++ Calculator.java
@@ -1,18 +1,29 @@
 public class Calculator
 {
-  public static void main(String[] args)
-  {
-    Calculator calc = new Calculator();
-	
-	double result = calc.add();
-	System.out.println(result);
-  }
+public static void main(String[] args)
+{
+ 
+Calculator myCalculator = new Calculator(); 
+
   
-   public double add()
-   {
-     int no1 =100, no2 =200;
-	 //int total = no1+no2;
-	 return 100+20.5;
-	 
-   }
+myCalculator.add(100); //100 - parameter
+myCalculator.add(100,200); 
+myCalculator.add(12.34f); 
+}
+
+
+public void add(float no) // argument
+{
+System.out.println(no+12.3); 
+}
+public void add(int no) // argument
+{
+System.out.println(no+123); 
+}
+public void add(int no,int no2) // argument
+{
+System.out.println(no+no2); 
+}
+
+
 }
