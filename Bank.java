846caa69091190aed5d97c6811c38731ced37037
veerakumar <veeravkmsccs@gmail.com>
2021-04-01 05:07:04 -0700

    Method calling from diffrent class

---
+++ Bank.java
@@ -0,0 +1,28 @@
+public class Bank
+{
+float interest = 4.3f; 
+private int salary = 25000;
+
+public void getLoan()
+{
+System.out.println("Getting loan");
+}
+
+public void openAccount()
+{
+System.out.println("Opening Account");
+}
+
+public void calculateInterest()
+{
+System.out.println("Calculating Interest");
+}
+private void training()
+{
+System.out.println("Training to employees"); 	
+}
+}
+
+
+
+
