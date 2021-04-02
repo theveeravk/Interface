9f94510d98fe10e0d4b486b8ea9593cce3e3dbe2
veerakumar <veeravkmsccs@gmail.com>
2021-04-02 05:59:19 -0700

    package creation

---
+++ Schools.java
@@ -0,0 +1,22 @@
+package chennai.velachery;
+
+public class Schools
+{
+	 int cycleFees = 20;
+	public static void main(String[] args)
+	{
+        System.out.println("School");
+	}
+	public  void admit()
+    {
+          System.out.println("Student admission"); 	
+    }
+    public void conductOnlineclass()
+    {
+    System.out.println("Online classes "); 	
+    }
+    public void teach()
+    {
+    System.out.println("Teaching"); 
+    }
+}
