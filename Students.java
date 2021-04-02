88e1ce1755317738565b6989c2a67afa6d6782ba
veerakumar <veeravkmsccs@gmail.com>
2021-04-02 05:53:38 -0700

    package creation

---
+++ Students.java
@@ -0,0 +1,14 @@
+package chennai.velachery;
+
+public class Students
+{
+  public static void main(String[] args)
+  {
+	  
+	  Schools sc = new Schools();
+	  System.out.println(sc.cycleFees);
+	  sc.admit();
+	  sc.conductOnlineclass();
+	  sc.teach();
+  }
+}
