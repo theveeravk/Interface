82f60d146ee5830ab688530b2058daf159f66fb0
veerakumar <veeravkmsccs@gmail.com>
2021-04-07 02:55:38 -0700

    Inheritance Ex-program-1 Headoffice

---
+++ HeadOffice.java
@@ -0,0 +1,17 @@
+class HeadOffice
+{
+public static void main(String[] args)
+{
+HeadOffice manager = new HeadOffice();
+}
+
+public void develop()
+{
+System.out.println("Developing Websites"); 
+}
+public void test()
+{
+System.out.println("Testing Websites"); 
+}
+
+}
