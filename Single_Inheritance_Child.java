f40b4f0e61fa47dacd305dec662a0cfd28da9cc1
veerakumar <veeravkmsccs@gmail.com>
2021-04-07 23:45:17 -0700

    Single  Inheritance Child class

---
+++ Single_Inheritance_Child.java
@@ -0,0 +1,13 @@
+public class Single_Inheritance_Child extends Single_Inheritance_Parent
+{
+	 
+	public static void main(String[] args)
+	{
+		Single_Inheritance_Child ch = new Single_Inheritance_Child();
+		ch.work();
+		ch.eat();
+		
+
+	}
+	
+}
