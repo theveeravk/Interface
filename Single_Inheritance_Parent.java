9da4508ac25c1676098b217c868d1ea7793541f0
veerakumar <veeravkmsccs@gmail.com>
2021-04-07 23:40:03 -0700

    Single Inheritance parent class

---
+++ Single_Inheritance_Parent.java
@@ -0,0 +1,15 @@
+public class Single_Inheritance_Parent
+{
+	
+ public static void main(String[] args)
+ {
+ }
+ public void work()
+ {
+   System.out.println("working nature of parent");
+ }
+ public void eat()
+ {
+   System.out.println("Eating Habits");
+ }
+}
