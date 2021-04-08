689257b2ec640c91460d34843192299d4d0625e0
veerakumar <veeravkmsccs@gmail.com>
2021-04-08 00:11:05 -0700

    Multilevel Inheritance GrandChild class

---
+++ Multilevel_Inheritance_GrandChild.java
@@ -0,0 +1,10 @@
+public class Multilevel_Inheritance_GrandChild extends  Multilevel_Inheritance_Child
+{
+	public static void main(String[] args)
+	{
+		 Multilevel_Inheritance_GrandChild  gc = new  Multilevel_Inheritance_GrandChild();
+		gc.work();
+		gc.eat();
+	}
+	
+}
