ce3c007aac0dcb90552c1f2736da55d1077d9da1
veerakumar <veeravkmsccs@gmail.com>
2021-04-08 00:08:05 -0700

    Multilevel Inheritance Child class

---
+++ Multilevel_Inheritance_Child.java
@@ -0,0 +1,14 @@
+public class Multilevel_Inheritance_Child extends Multilevel_Inheritance_Parent
+{
+	 
+	public static void main(String[] args)
+	{
+		Multilevel_Inheritance_Child ch = new Multilevel_Inheritance_Child();
+		ch.work();
+		ch.eat();
+		
+
+	}
+	
+	
+}
