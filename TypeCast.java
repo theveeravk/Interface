dba6f61896fd139d5cebc445e4eb6dc80b10e139
veerakumar <veeravkmsccs@gmail.com>
2021-04-13 06:05:26 -0700

    Type Casting

---
+++ TypeCast.java
@@ -0,0 +1,10 @@
+public class TypeCast 
+{
+  public static void main(String[] args) {
+    double value = 8.98;
+    int output = (int) value; // Manual casting: double to int
+
+    System.out.println(value);   
+    System.out.println(output);     
+  }
+}
